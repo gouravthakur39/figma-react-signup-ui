141a6ce7c1bbf30892c638e2d6c158dccff8bc6d
gouravthakur39 <gourav.thakur39@gmail.com>
2021-11-11 13:09:14 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+import bgImg from "./assets/bg.png";
+
+const App = () => {
+  return (
+    <h1>start</h1>
+  )
+}
+
+export default App;
